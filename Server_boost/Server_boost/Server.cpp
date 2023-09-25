e746b4ce035aec0693f5e74c1f37c48ddfd1cfdb
Nursultan <102341503+Joldoshov11@users.noreply.github.com>
2023-09-25 09:28:33 +0600

    2

--- Server_boost/Server_boost/Server.cpp
+++ Server_boost/Server_boost/Server.cpp
@@ -15,13 +15,11 @@ int main() {
 	while (true)
 	{
 		std::unique_lock<std::mutex> m_lock(m_mutex);
-		c_variable.wait(m_lock);
+		c_variable.wait(m_lock);	
 	}
 
 	return 0;
 }
-
-
 
 
 bool Server::Start() {
@@ -33,11 +31,11 @@ bool Server::Start() {
 	}
 	catch (const std::exception&)
 	{
-		printlog("Server: Error waitclient!!!");
+		printlog("Error waitclient!!!");
 		return false;
 	}
 
-	printlog("Server: Stared!!!");
+	printlog("Stared!!!");
 	return true;
 }
 
@@ -47,17 +45,70 @@ void Server::Stop() {
 		context_thread.join();
 }
 
+void Connect::send(const std::string& message) {
+	write_message = message;
+	set_size_to_bit_array(write_size, write_message.size());
+	boost::asio::async_write(socket, boost::asio::buffer(write_size),
+		[this](const boost::system::error_code& error, size_t bytes_transferred) {
+			if (!error) {
+				boost::asio::async_write(socket, boost::asio::buffer(write_message), 
+					[](const boost::system::error_code& error_, size_t bytes_transferred_) {
+						if (!error_) {
+							printlog("Success send");
+						}
+						else {
+							printlog("Error by send message");
+						}
+					});
+			}
+			else {
+				printlog("Error by send size");
+			}
+		});
+}
+
 void Server::WaitClientConnect() {
 	acceptor.async_accept(
 		[this](std::error_code error, boost::asio::ip::tcp::socket socket) {
 			if (error) {
-				printlog("Server: Connect error!!!");
+				printlog("Connect error!!!");
 			}
 			else {
-				printlog("Server: New Client!!!");
+				printlog("New Client!!!");
 				std::unique_ptr<Connect> tmp = std::make_unique<Connect>(std::move(socket), startID++);
+				tmp->read();
 				Clients_Array.push_back(std::move(tmp));
+				
 			}
+			
 			WaitClientConnect();
 		});
 }
+
+void Connect::read() {
+	if (socket.is_open()) {
+		boost::asio::async_read(socket, boost::asio::buffer(read_size),
+			[this](const boost::system::error_code& error, std::size_t bytes_transferred) {
+				if (!error) {
+					size_t size = get_size_from_bit_array(read_size);
+					write_message.resize(size);
+					boost::asio::async_read(socket, boost::asio::buffer(write_message),
+						[this](const boost::system::error_code& error_, std::size_t bytes_transferred_) {
+							if (!error_) {
+								printlog(write_message);
+							}
+							else {
+								printlog("Error by read message");
+							}
+						});
+				}
+				else {
+					printlog("Error by read size");
+				}
+			});
+	}
+	else {
+		printlog("Socket close");
+	}
+}
+
--- Server_boost/Server_boost/Server.h
+++ Server_boost/Server_boost/Server.h
@@ -10,25 +10,48 @@
 #include <boost/asio/ts/internet.hpp>
 #include <boost/asio/error.hpp>
 #include <boost/system/error_code.hpp>
+#include <boost/smart_ptr/enable_shared_from_this.hpp>
+#include <boost/bind.hpp>
 #include <thread>
 #include <vector>
+#include <bitset>
 
 
 template<typename T>
-void printlog(const T& message) {
-	std::cout << "[" << message << "]" << std::endl;
+static void printlog(const T& message) {
+	std::cout << "[Server: " << message << "]" << std::endl;
 }
 
 
-class Connect {
+void set_size_to_bit_array(std::uint16_t *write_size, const size_t& size) {
+	std::memcpy(write_size, &size, sizeof(size_t));
+}
+
+size_t get_size_from_bit_array(std::uint16_t* write_size) {
+	size_t return_size;
+	std::memcpy(&return_size, write_size, sizeof(size_t));
+	return return_size;
+}
+
+
+class Connect : public boost::enable_shared_from_this<Connect> {
 public:
-	Connect(boost::asio::ip::tcp::socket _socket, uint16_t _ID) : socket(std::move(_socket)), ID(_ID) {}
+	Connect(boost::asio::ip::tcp::socket _socket, uint16_t _ID) : socket(std::move(_socket)), ID(_ID) {
+	}
+	void read();
+	void send(const std::string& message);
+private:
+	std::string write_message;
+	std::uint16_t write_size[sizeof(size_t)];
+	std::uint16_t read_size[sizeof(size_t)];
+	std::string read_message;
 	boost::asio::ip::tcp::socket socket;
 	uint16_t ID;
+
 };
 
 
-class Server {
+class Server : public boost::enable_shared_from_this<Server> {
 
 public:
 	Server(const uint16_t& port) : acceptor(context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {}
@@ -47,5 +70,4 @@ private:
 	uint16_t startID = 1;
 
 	void WaitClientConnect();
-
 };
